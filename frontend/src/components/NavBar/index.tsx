a7106fa309b988c0d189a9308c12cf0e27df6b0e
Matheus Ramos <devmatheusr@gmail.com>
2022-07-25 23:18:37 -0300

    desconsiderar

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, NavDropdown, FormControl } from 'react-bootstrap';
 
 
 
@@ -14,33 +14,35 @@ import './styles.css'
 function NavBar() {
     return (
         <>
-            <Navbar bg={theme.background} expand="md">
-                <Container>
-                    <Navbar.Brand href="#home">Balanced Nature</Navbar.Brand>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
 
-                </Container>
-                <Container>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    </Navbar.Collapse>
-                    <Nav className='justify-content-end'>
-                        <Form className="d-flex">
-                            <Form.Control
-                                type="search"
-                                placeholder="Search"
-                                className="me-2"
-                                aria-label="Search"
-                            />
-                        </Form>
-
-                        <Button variant="secondary" size="sm">
-                            Login
-                        </Button>
-                        <Button variant="secondary" size="sm">
-                            Registrar
-                        </Button>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
-                </Container>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                    <span className="Nav">
+                        <Nav className="justify-content-center">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#home">Product</Nav.Link>
+                            <Nav.Link href="#home">Solutions</Nav.Link>
+                            <Nav.Link href="#home">Resources</Nav.Link>
+                        </Nav>
+                    </span>
+                </Navbar.Collapse>
             </Navbar>
         </>
     )
